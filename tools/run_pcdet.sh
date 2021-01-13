2bfe76126d00a09b9d92539228b8b997f26026a8
Juncong Fei <juncong.fei@ext.mpsa.com>
2021-01-13 21:38:35 +0100

    pillar_obs_dense_concat_simunet_wda1, weight 5 8, translate 5,5

--- tools/run_pcdet.sh
+++ tools/run_pcdet.sh
@@ -3,4 +3,4 @@
 #CUDA_VISIBLE_DEVICES=1 python test.py --cfg_file cfgs/nuscenes_models/cbgs_pp_multihead.yaml --extra_tag 'pillar_obs_dense_att_simunet_wda' --eval_all
 
 # pillar_obs_dense_concat_simunet_wda
-CUDA_VISIBLE_DEVICES=0 python train.py --cfg_file cfgs/nuscenes_models/cbgs_pp_multihead.yaml --extra_tag 'pillar_obs_dense_concat_simunet_wda'
+CUDA_VISIBLE_DEVICES=0 python train.py --cfg_file cfgs/nuscenes_models/cbgs_pp_multihead.yaml --extra_tag 'pillar_obs_dense_concat_simunet_wda1'
